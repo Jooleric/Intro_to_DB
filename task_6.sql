0e46aa9696ef8150ec65c9b95c6dbd4cb5bb42b1
Jooleric <bernardjoseph7872gmail.com>
2025-09-05 18:34:43 +0100

    Add task_6: insert customers 2,3,4

--- task_6.sql
+++ task_6.sql
@@ -1,12 +1,9 @@
--- Use the correct database
 USE alx_book_store;
 
--- Delete existing rows with these IDs to avoid duplicate primary key errors
 DELETE FROM customers WHERE customer_id IN (2, 3, 4);
 
--- Insert multiple rows at once
 INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+VALUES
+  (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+  (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+  (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
